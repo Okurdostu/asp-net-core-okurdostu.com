fb2ad57972588a58e18ccb4d246f4f1723fe5da5
halilkocaoz <halilikcz@gmail.com>
2020-06-17 16:27:53 +0300

    layout implementation.returning userprofile with route constraint

--- src/Okurdostu.Web/Controllers/ProfileController.cs
+++ src/Okurdostu.Web/Controllers/ProfileController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Okurdostu.Data;
 
 namespace Okurdostu.Web.Controllers
 {
     public class ProfileController : Controller
     {
-        public IActionResult Index(string username)
+        private readonly OkurdostuContext Context;
+        public ProfileController(OkurdostuContext _context) => Context = _context;
+
+        public async Task<IActionResult> Index(string username)
         {
-            return View();
+            if (!string.IsNullOrEmpty(username))
+            {
+                var User = await Context.User.FirstOrDefaultAsync(x => x.Username == username && x.IsActive);
+                if (User != null) return View(User);
+            }
+
+            //404
+            return null;
         }
     }
 }
