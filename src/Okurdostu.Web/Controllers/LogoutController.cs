3d1237530cf3bae1f7a8d06d551742b96c0f0dc4
halilkocaoz <halil.i.kocaoz@gmail.com>
2020-06-23 17:35:46 +0300

    usermenu, logout added

---
+++ src/Okurdostu.Web/Controllers/LogoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Okurdostu.Web.Controllers
+{
+    public class LogoutController : Controller
+    {
+        [HttpPost,ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect("/");
+        }
+    }
+}
