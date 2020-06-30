1078678561115f8e9b0eb05245f905b99370a1c0
halilkocaoz <halil.i.kocaoz@gmail.com>
2020-07-01 02:03:47 +0300

    added open, completed need campaigns into user profile
    not added design.

---
+++ src/Okurdostu.Web/ViewComponents/CompletedNeedsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Okurdostu.Data;
+using Okurdostu.Data.Model;
+using Okurdostu.Web.Models;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Okurdostu.Web.ViewComponents
+{
+    [ViewComponent(Name = "CompletedNeeds")]
+    public class CompletedNeedsViewComponent : ViewComponent
+    {
+        
+        private readonly OkurdostuContext Context;
+        public CompletedNeedsViewComponent(OkurdostuContext _context) => Context = _context;
+        public async Task<IViewComponentResult> InvokeAsync(long UserId)
+        {
+
+            var CompletedNeedList = await Context.Need.Where(x => !x.IsRemoved && x.UserId == UserId && x.IsCompleted).ToListAsync();
+            return View(CompletedNeedList);
+
+        }
+
+    }
+}
+
+
